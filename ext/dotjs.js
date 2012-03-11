c7fd95f08947551c065b5fdd54772505ca0014ab
Marcin Bunsch <marcin@futuresimple.com>
2012-03-11 12:11:46 +0100

    Bringing back jquery, mixing the approaches

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,7 +1,12 @@
-// Append a script tag instead of evaluating as the extenstion runtime is getting in the way
-var script  = document.createElement('script')
-script.src  = 'http://localhost:3131/'+window.location.host.replace('www.','')
-script.type = 'text/javascript'
-window.onload = function() {
-  document.body.appendChild(script);
-};
+$.ajax({
+  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'',
+  dataType: 'text',
+  success: function(d){
+    var script  = document.createElement('script')
+    script.innerHTML = d
+    document.body.appendChild(script);
+  },
+  error: function(){
+    console.log('no dotjs server found at localhost:3131')
+  }
+})
